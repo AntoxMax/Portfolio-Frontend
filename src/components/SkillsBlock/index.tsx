557dbb6121943f8bad7589fa4aab12143b6cc22e
Maksimkin Anon <naksimkin.antoxa@gmail.com>
2024-01-07 14:00:28 +0900

    Верстка второго блока

--- src/components/SkillsBlock/index.tsx
+++ src/components/SkillsBlock/index.tsx
@@ -1,10 +1,29 @@
-import React from "react";
 import VerticalLayout from "../../layouts/VerticalLayout";
+import { BlockTitle } from "../../ui/blockTitle/BlockTitle";
+import s from "./style.module.scss";
 
 export const SkillsBLock = () => {
   return (
     <VerticalLayout>
-      <div></div>
+      <BlockTitle>Навыки</BlockTitle>
+      <div className={s.skills}>
+        <div className={s.skills__block}>
+          <div className={s.skills__text}>Frontend:</div>
+          <div className={s.skills__items}>
+            <div className={s.skills__item}>React</div>
+            <div className={s.skills__item}>Redux</div>
+            <div className={s.skills__item}>Css</div>
+            <div className={s.skills__item}>HTML</div>
+          </div>
+          <div className={s.skills__text}>Другое:</div>
+          <div className={s.skills__items}>
+            <div className={s.skills__item}>Wordpress</div>
+            <div className={s.skills__item}>Node</div>
+            <div className={s.skills__item}>Express</div>
+            <div className={s.skills__item}>Figma</div>
+          </div>
+        </div>
+      </div>
     </VerticalLayout>
   );
 };
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { MainFirstBlock } from "../components/MainFirstBlock";
+import { SkillsBLock } from "../components/SkillsBlock";
 
 export const Main = () => {
   return (
     <>
       <MainFirstBlock />
+      <SkillsBLock />
     </>
   );
 };
